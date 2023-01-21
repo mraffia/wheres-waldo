f3a8baead4cfb8340053406ad77c6220b133c230
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-22 01:42:07 +0700

    Add Link to each level so it routes to each respective game

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../styles/GameCard.css';
 
 function GameCard({ level, handlePickLevel }) {
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/HomePage.css';
 import GameCard from './GameCard.js';
 
@@ -10,7 +11,9 @@ function HomePage({ levels, handlePickLevel }) {
           levels.map((level, i) => {
             return (
               <div key={i}>
-                <GameCard level={level} handlePickLevel={handlePickLevel} />
+                <Link to="/game" className="game-link">
+                  <GameCard level={level} handlePickLevel={handlePickLevel} />
+                </Link>
               </div>
             )
           })
