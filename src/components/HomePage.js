7433966ea09673f0d54387c22deb94d07814c652
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-21 23:20:07 +0700

    Add HomePage component

---
+++ src/components/HomePage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/HomePage.css';
+import GameCard from './GameCard.js';
+
+function HomePage({ levels, handlePickLevel }) {
+  return (
+    <div className="home-container">
+        <div className="home-subcontainer">
+          {levels.map((level, i) => {
+            return (
+              <div key={i}>
+                <GameCard level={level} handlePickLevel={handlePickLevel} />
+              </div>
+            )
+          })}
+        </div>
+    </div>
+  );
+}
+
+export default HomePage;
