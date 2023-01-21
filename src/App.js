33a7f966dc7429a6d07e6dadf54c916e834dd8a1
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-21 20:46:32 +0700

    Setup firebase with cloud firestore and cloud storage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  limit,
+  onSnapshot,
+  setDoc,
+  updateDoc,
+  doc,
+  serverTimestamp,
+} from 'firebase/firestore';
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage';
+import { getFirebaseConfig } from './firebase-config.js';
 import './App.css';
 
+const firebaseAppConfig = getFirebaseConfig();
+const app = initializeApp(firebaseAppConfig);
+
 function App() {
+
   return (
     <div className="container">
     </div>
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+const config = {
+  apiKey: "AIzaSyDczK1fCD_GKItCULc_QqR_11d748irwaE",
+  authDomain: "whereswaldomraffia.firebaseapp.com",
+  projectId: "whereswaldomraffia",
+  storageBucket: "whereswaldomraffia.appspot.com",
+  messagingSenderId: "489609000513",
+  appId: "1:489609000513:web:891dbd46cf49ca3bd838c0"
+};
+
+export function getFirebaseConfig() {
+  if (!config || !config.apiKey) {
+    throw new Error('No Firebase configuration object provided.' + '\n' +
+    'Add your web app\'s configuration object to firebase-config.js');
+  } else {
+    return config;
+  }
+}
