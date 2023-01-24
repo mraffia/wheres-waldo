7df93051b8b96db4f9d5bb48c4f3ecb7bb10e909
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-24 14:46:59 +0700

    Add heading to HomePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,6 +6,7 @@ import GameCard from './GameCard.js';
 function HomePage({ levels, handlePickLevel }) {
   return (
     <div className="home-container">
+      <h1>Choose A Level:</h1>
       <div className="home-subcontainer">
         {levels.map((level, i) => {
           return (
