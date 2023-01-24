77541a0ff2aa62e546f292710be5d71352861b8a
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-25 05:19:02 +0700

    Add logic to GamePage so that when isLoading props is true, it renders a react-loader-spinner component, also when isError props is true, it renders an error message

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,10 +1,11 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Popup from 'reactjs-popup';
+import { Rings } from 'react-loader-spinner';
 import 'reactjs-popup/dist/index.css';
 import '../styles/GamePage.css';
 
-function GamePage({ level, handleSubmitToLeaderboard }) {
+function GamePage({ level, handleSubmitToLeaderboard, isLoading, isError }) {
   const [openCharacterDropdown, setOpenCharacterDropdown] = useState(false);
   const [cursorPosition, setCursorPosition] = useState({ left: 0, top: 0 });
   const [activeAreas, setActiveAreas] = useState({
@@ -84,68 +85,84 @@ function GamePage({ level, handleSubmitToLeaderboard }) {
 
   return (
     <div className="game-container">
-        <div className="game-characters">
-          {level.characters.map((character, i) => {
-            return (
-              <div key={i} className="game-character-container" style={foundStatus[level.difficulty][character.name] ? { opacity: 0.3 } : { opacity: 1 }}>
-                <img className="game-character-image" src={character.image} alt={character.name} />
-                <div className="game-character-name"><strong>{character.name}</strong></div>
-              </div>
-            )
-          })}
-        </div>
-        <div className="game-image-container">
-          <div className="game-image-subcontainer">
-            <img className="game-image" src={level.image} alt={level.difficulty} onClick={(e) => handleClickDropdownAppear(e)} />
+      {isError && <div className="error-message">Something went wrong ...</div>}
+      {isLoading ? (
+        <Rings
+          height="100"
+          width="100"
+          color="rgba(17, 45, 78, 1)"
+          radius="1"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="rings-loading"
+        />
+      ) : (
+        <div className="game-subcontainer">
+          <div className="game-characters">
             {level.characters.map((character, i) => {
-                return (
-                  <div 
-                    key={i} 
-                    id={level.difficulty + "-" + character.name} 
-                    className="game-image-character-area" 
-                    onClick={(e) => handleClickDropdownAppear(e, character)}>
-                  </div>
-                )
-              })}    
+              return (
+                <div key={i} className="game-character-container" style={foundStatus[level.difficulty][character.name] ? { opacity: 0.3 } : { opacity: 1 }}>
+                  <img className="game-character-image" src={character.imageUrl} alt={character.name} />
+                  <div className="game-character-name"><strong>{character.name}</strong></div>
+                </div>
+              )
+            })}
           </div>
-          {openCharacterDropdown && (
-            <div className="dropdown-characters" style={{left: cursorPosition.left + "px", top: cursorPosition.top + "px"}}>
+          <div className="game-image-container">
+            <div className="game-image-subcontainer">
+              <img className="game-image" src={level.imageUrl} alt={level.difficulty} onClick={(e) => handleClickDropdownAppear(e)} />
               {level.characters.map((character, i) => {
-                return (
-                  <div key={i} className="dropdown-character-container" onClick={() => { handleChooseCharacter(character, time) }}>
-                    <img className="dropdown-character-image" src={character.image} alt={character.name} />
-                    <div className="dropdown-character-name"><strong>{character.name}</strong></div>
-                  </div>
-                )
-              })}
+                  return (
+                    <div 
+                      key={i} 
+                      id={level.difficulty + "-" + character.name} 
+                      className="game-image-character-area" 
+                      onClick={(e) => handleClickDropdownAppear(e, character)}>
+                    </div>
+                  )
+                })}    
             </div>
-          )}
-        </div>
-        <Popup
-          open={foundAll}
-          modal
-          closeOnDocumentClick={false}
-          closeOnEscape={false}
-        >
-          <div className="modal">
-            <div className="header"><strong>You finished in { Math.floor((time / 1000)) + "." + ((time / 10) % 100) + " seconds!"}</strong></div>
-            <div className="content">
-              Enter your name to save your score on the leaderboard.
-            </div>
-            <div className="actions">
-              <label htmlFor="name">Name:</label>
-              <input type="text" id="name" name="name" className="input" placeholder="Your name/alias" onChange={(e) => handleNameChange(e)}></input>
-              <div className="button-container">
-                <Link to="/leaderboard">
-                  <button className="button btn-submit" onClick={() => handleSubmitToLeaderboard(playerName, time, level.difficulty)}>Submit score</button>
-                </Link>
-                <Link to="/">
-                  <button className="button btn-danger">Cancel</button>
-                </Link>
+            {openCharacterDropdown && (
+              <div className="dropdown-characters" style={{left: cursorPosition.left + "px", top: cursorPosition.top + "px"}}>
+                {level.characters.map((character, i) => {
+                  return (
+                    <div key={i} className="dropdown-character-container" onClick={() => { handleChooseCharacter(character, time) }}>
+                      <img className="dropdown-character-image" src={character.imageUrl} alt={character.name} />
+                      <div className="dropdown-character-name"><strong>{character.name}</strong></div>
+                    </div>
+                  )
+                })}
               </div>
+            )}
+          </div>
+        </div>
+      )}
+      <Popup
+        open={foundAll}
+        modal
+        closeOnDocumentClick={false}
+        closeOnEscape={false}
+      >
+        <div className="modal">
+          <div className="header"><strong>You finished in { Math.floor((time / 1000)) + "." + ((time / 10) % 100) + " seconds!"}</strong></div>
+          <div className="content">
+            Enter your name to save your score on the leaderboard.
+          </div>
+          <div className="actions">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" className="input" placeholder="Your name/alias" onChange={(e) => handleNameChange(e)}></input>
+            <div className="button-container">
+              <Link to="/leaderboard">
+                <button className="button btn-submit" onClick={() => handleSubmitToLeaderboard(playerName, time, level.difficulty)}>Submit score</button>
+              </Link>
+              <Link to="/">
+                <button className="button btn-danger">Cancel</button>
+              </Link>
             </div>
           </div>
-        </Popup>
+        </div>
+      </Popup>
     </div>
   );
 }
