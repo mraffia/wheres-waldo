cbb75259bc1cb5e1c7196a89bea492518b0acfe4
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-25 05:17:36 +0700

    Add logic to HomePage so that when isLoading props is true, it renders a react-loader-spinner component, also when isError props is true, it renders an error message

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,23 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Rings } from 'react-loader-spinner';
 import '../styles/HomePage.css';
 import GameCard from './GameCard.js';
 
-function HomePage({ levels, handlePickLevel }) {
+function HomePage({ levels, handlePickLevel, isLoading, isError }) {
   return (
     <div className="home-container">
       <h1>Choose A Level:</h1>
-      <div className="home-subcontainer">
-        {levels.map((level, i) => {
-          return (
-            <div key={i}>
-              <Link to="/game" className="game-link">
-                <GameCard level={level} handlePickLevel={handlePickLevel} />
-              </Link>
-            </div>
-          )
-        })}
-      </div>
+      {isError && <div className="error-message">Something went wrong ...</div>}
+      {isLoading ? (
+        <Rings
+          height="100"
+          width="100"
+          color="rgba(17, 45, 78, 1)"
+          radius="1"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="rings-loading"
+        />
+      ) : (
+        <div className="home-subcontainer">
+          {levels.map((level, i) => {
+            return (
+              <div key={i}>
+                <Link to="/game" className="game-link">
+                  <GameCard level={level} handlePickLevel={handlePickLevel} />
+                </Link>
+              </div>
+            )
+          })}
+        </div>
+      )}
     </div>
   );
 }
